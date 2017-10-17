fed7f1759130b67006812dc112389deb1fdfc31c
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-17 12:06:57 -0500

    answered drills 1 through 7

--- drills.js
+++ drills.js
@@ -1,23 +1,89 @@
 'use strict';
 
+require('dotenv').config();
 const { DATABASE } = require('./config');
-const knex = require('knex')(DATABASE)({
+const knex = require('knex')({
   client: 'pg',
-  connection: {
-    database: 'dev-restaurants-app'
-  },
+  connection: process.env.DATABASE_URL
 });
 
 // clear the console before each run
 process.stdout.write('\x1Bc');
 
 // Sample select 
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
+
+//1
+// knex
+//   .select()
+//   .table('restaurants')
+//   .then(res => console.log(res))
+
+
+//2
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where( {cuisine: 'Italian'} )
+//   .then(res => console.log(res));
+
+//3
+// knex
+//   .select('id', 'name')
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .limit(10)
+//   .then(res => console.log(res));
+
+//4
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where('cuisine', 'Thai')
+//   .count()
+//   .then(res => console.log(res));
+
+//5
+// knex
+//   .select()
+//   .from('restaurants')
+//   .count()
+//   .then(res => console.log(res))
+
+//6
+// knex 
+  // .select()
+  // .from('restaurants')
+  // .where({cuisine: 'Thai', address_zipcode: 11372})
+  // .count()
+  // .then(res => console.log(res))
+
+//7
 knex
-  .select()
+  .select('id', 'name')
   .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+  .where({cuisine: 'Italian', address_zipcode: (10012, 10013, 10014)})
+  .limit(5)
+  .then(res => console.log(res))
+
+//8
+
+//9
+
+//10
+
+//11
+
+//12
+
+//13
+
 
 
 // Destroy the connection pool
