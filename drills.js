511f67bbf858771918e52fcf12e58e5035928cd8
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-17 11:06:04 -0500

    initial commit

---
+++ drills.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { DATABASE } = require('./config');
+const knex = require('knex')(DATABASE)({
+  client: 'pg',
+  connection: {
+    database: 'dev-restaurants-app'
+  },
+});
+
+// clear the console before each run
+process.stdout.write('\x1Bc');
+
+// Sample select 
+knex
+  .select()
+  .from('restaurants')
+  .limit(2)
+  .debug(true)
+  .then(results => console.log(results));
+
+
+// Destroy the connection pool
+knex.destroy().then(() => {
+  console.log('database connection closed');
+});
